d1bdad10fdb36cefc61f2b134b023755c137a66d
TheGoldenMine <86381367+TheGhostRoot@users.noreply.github.com>
2023-02-02 14:11:50 +0200

    Update HashTable.py

--- HashTable.py
+++ HashTable.py
@@ -1,13 +1,10 @@
 class HashTable:
-
     # Create empty bucket list of given size
     def __init__(self, size):
         self.size = size
         self.hash_table = self.create_buckets()
-
     def create_buckets(self):
         return [[] for _ in range(self.size)]
-
     # Insert values into hash map
     def set_val(self, key, val):
 
